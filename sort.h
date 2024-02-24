85db4d96993e710af371c8d9ba93ec2f7e6ebd8c
MikeMalindzisa <machaweml@gmail.com>
2024-02-24 05:08:33 -0800

    header

--- sort.h
+++ sort.h
@@ -28,6 +28,7 @@ int len_list(listint_t *h);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void swap(int *a, int *b);
+int locate_min(int *array, size_t index, size_t size);
 int lomuto_partition(int *array, int low, int high, size_t size);
 void quicksort_recursive(int *array, int low, int high, size_t size);
 void quick_sort(int *array, size_t size);
